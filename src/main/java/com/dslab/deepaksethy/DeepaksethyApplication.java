255be40ac673273b82f6c7abd3c27bc294c2d2a4
ds10805 <SethyDeepakRanjan@JohnDeere.com>
2018-06-18 04:06:24 +0530

    war issue fixed

--- src/main/java/com/dslab/deepaksethy/DeepaksethyApplication.java
+++ src/main/java/com/dslab/deepaksethy/DeepaksethyApplication.java
@@ -2,11 +2,20 @@ package com.dslab.deepaksethy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeepaksethyApplication {
+public class DeepaksethyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeepaksethyApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DeepaksethyApplication.class);
+		
+	}
+	
 }
