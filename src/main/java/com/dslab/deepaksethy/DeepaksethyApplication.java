8c393bb52f29e4fe222ebe4bacf1154a3826f52c
deepakrsethy <deepakrsethy@gmail.com>
2018-06-18 03:01:47 +0530

    origin

---
+++ src/main/java/com/dslab/deepaksethy/DeepaksethyApplication.java
@@ -0,0 +1,12 @@
+package com.dslab.deepaksethy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepaksethyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepaksethyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dslab/deepaksethy/DeepaksethyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dslab.deepaksethy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeepaksethyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
